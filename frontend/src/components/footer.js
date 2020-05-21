5339acb03d729ef75331fc0670dca1798e30f70a
Gabrielle Chan <yccgabby@Gabrielles-MacBook-Pro.local>
2020-05-21 04:08:59 -0400

    add footer

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render() {
+        return(
+            <div style={{backgroundColor: '#252627'}}>
+                <footer style={{textAlign: 'center', display: 'flex', justifyContent: 'center', backgroundColor: '#252627', paddingBottom: '0', paddingTop: '10px'}}>
+                    <div style={{backgroundColor: '#252627'}}>
+                        <div style={{textAlign: 'center', color: '#aaaaaa',backgroundColor: '#252627', fontFamily: 'Roboto'}}>
+                            <p> &copy; Made with love by Gabby Chan</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Footer;
