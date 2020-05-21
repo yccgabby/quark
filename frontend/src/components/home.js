3f67bdb5fa5b9eace2ea6a1fa8b1d688db98a0a7
Gabrielle Chan <yccgabby@Gabrielles-MacBook-Pro.local>
2020-05-21 04:09:58 -0400

    finish homepage

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,22 +1,63 @@
 import React from 'react';
+import Typical from 'react-typical';
+import bggif from './images/bg.gif';
 
 class Home extends React.Component {
     render() {
         return (
-            <header>
-                <div style={headerStyle} id="home">
-                    <h1>Quark</h1>
-                    <h2>Let us find your perfect style</h2>
+                <div style={{position: 'relative', outline: '0px'}}>
+                    <img src={bggif} alt="" style={{minWidth: '100%', minHeight: '90vh', class: 'center', marginLeft: 'auto', marginRight: 'auto', display: 'block'}}/>
+                    <div style={rectHeader}></div>
+                    <h1 style={headerStyle}>personalized <a href='https://www.thegoodtrade.com/features/thrift-shopping-tips' style={{color:'#aaaaaa'}} target="_blank" rel="noopener noreferrer">thrifting</a> experience</h1>
+                    <h2 style={headerStyle.smallerStyle}>
+                        <Typical steps={[
+                            'custom recommendations',
+                            1000,
+                            'unique, vintage finds',
+                            1000,
+                            'sustainable fashion',
+                            1000,
+                            'let us find your style'
+                        ]}
+                        wrapper='i'
+                        />
+                    </h2>
                 </div>
-            </header>
         )
     }
 }
 
+const rectHeader = {
+    position: 'absolute',
+    top: '28%',
+    left: '12%',
+    height: '19vmin 20vmax',
+    width: '76vw',
+    backgroundColor: '#252627'
+}
+
 const headerStyle = {
-    color: '#ff734f',
+    position: 'absolute',
+    top: '37%',
+    left: '12%',
+    color: '#0b4f6c',
     textAlign: 'center',
-    padding: '40px',
+    fontFamily: 'Roboto',
+    fontSize: '9vh',
+    paddingLeft: '8%',
+    paddingRight: '16%',
+    textShadow: '3px 3px 20px #000',
+    smallerStyle: {
+        position: 'absolute',
+        bottom: '10%',
+        left: '10%',
+        color: '#e0503f',
+        textAlign: 'center',
+        width: '80%',
+        textShadow: '3px 3px 10px #000',
+        paddingBottom: '9%',
+        fontSize: '4vh'
+    }
 };
 
 export default Home;
