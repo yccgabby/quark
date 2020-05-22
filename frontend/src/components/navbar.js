91ed9be6810b2ceb23c2f8ded3942202b5c2550b
Gabrielle Chan <yccgabby@Gabrielles-MacBook-Pro.local>
2020-05-22 03:36:31 -0400

    sticky nav bar + text shadow

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 class MyNavBar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light" style={navStyle}> {/* make it sticky top later */}
+            <nav className="navbar navbar-expand-lg sticky-top navbar-light" style={navStyle}> 
                 <a className="navbar-brand" href="/" style={wordStyle}>
                     <img src={img} width="40" height="40" className="d-inline-block align-center" alt=""></img>quark
                     </a>
@@ -29,18 +29,21 @@ class MyNavBar extends React.Component {
 
 const navStyle = {
     backgroundColor: '#252627',
-    padding: '5px'
+    padding: '5px',
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
 }
 
 const navItemWordStyle = {
     color: '#e0503f',
-    fontSize: 20
+    fontSize: 20,
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
 }
 
 const wordStyle = {
     color: '#e0503f',
     fontSize: 40,
-    marginLeft: 20
+    marginLeft: 20,
+    textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
 }
 
 export default MyNavBar;
